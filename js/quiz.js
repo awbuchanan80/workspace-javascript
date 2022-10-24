5f659dc445f79afccdd5028fd7ef15f4864eb01c
Adam Buchanan <adamwb@usca.edu>
2022-10-24 13:13:14 -0400

    Classwork 10/24/22

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+//event handler
+function changestyle(){
+    var classname = this.getAttribute("class");
+    if(classname == "answer"){
+        this.setAttribute("class", "answer selected");
+    }else{
+        this.setAttribute("class", "answer");
+    }
+    
+}
+
+for(var i = 0; i < 4; i++){
+        document.getElementsByClassName('answer')[i].addEventListener('click', changestyle, false);
+}
