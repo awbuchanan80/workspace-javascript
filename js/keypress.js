ad5ab231ca31d0c7f4cf3416b626859fa1640c66
Adam Buchanan <adamwb@usca.edu>
2022-11-02 12:31:00 -0400

    Classwork 11/2/2022

---
+++ js/keypress.js
@@ -0,0 +1,16 @@
+//event
+//var messageE = document.getElementById('message');
+//messageE.addEventListener('keyup', showKey);
+$('#message').keyup(showKey);
+document.addEventListener('mousemove',showPosition);
+
+//event handler
+function showKey(e){
+    console.log(e.key);
+    document.getElementById('lastKey').textContent="The key is "+ e.key;
+}
+
+function showPosition(e){
+    console.log("The position is "+e.pageX+", "+e.pageY);
+    document.getElementById('lastKey').textContent="The position is "+e.pageX+", "+e.pageY;
+}
