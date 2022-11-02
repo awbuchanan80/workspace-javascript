c171d26473b2c464771e8f02ad0b40fe1adecf92
Adam Buchanan <adamwb@usca.edu>
2022-11-02 13:02:54 -0400

    Homework 11/2/2022

--- js/keypress.js
+++ js/keypress.js
@@ -14,3 +14,9 @@ function showPosition(e){
     console.log("The position is "+e.pageX+", "+e.pageY);
     $('#lastKey').text=("The position is "+e.pageX+", "+e.pageY);
 }
+
+const canvas = document.getElementById("playground");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle="green";
+ctx.fillRect(10,10,150,100);
