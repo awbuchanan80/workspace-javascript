d770641ae99d4d20a4f7a5e73f23309ed600469a
Adam Buchanan <adamwb@usca.edu>
2022-11-07 12:26:47 -0500

    edit

--- js/hotel.js
+++ js/hotel.js
@@ -11,6 +11,7 @@ console.log(test.hotelname);
 console.log(test.room);
 $(function(){
     $('#submit').click(reservation);
+    $('#lists').click(displayR);
 })
 
 var guestgroup = new Array();
@@ -22,6 +23,19 @@ function reservation(){
         checkOut: $('#checkout').val(),
         roomNo: $('#roomNo').val()
     };
+    $("p:first").html("Thank you for your reservation!");
     guestgroup.push(guest);
     console.log(guestgroup);
+}
+
+function displayR(){
+    $("form").append("<table>");
+    for(var i in displayR){
+        var theguest = displayR[i];
+        var row = "<tr><td>"+theguest.name+"</td>"+"<td>"+theguest.numberofGuest+"</td>"+"<td>"+theguest.checkin+"</td>"+"<td>"+theguest.checkout+"</td>"+"<td>"+theguest.roomNo+"</td></tr>";
+        $("table").append(row);
+    }
+
+
+
 }
